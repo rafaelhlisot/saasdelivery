b41ed464c9caaf5f08fe5d5f814fa3e162d13076
Rafael Henrique Lisot <rafael.lisot@unoesc.edu.br>
2022-07-17 16:16:22 -0300

    pagina login

---
+++ components/Header/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.css';
+import BackIcon from './backIcon.svg';
+import Link from 'next/link';
+
+type Props = {
+  backHref: string;
+  color: string;
+  title?: string;
+  subtitle?: string;
+}
+
+export const Header = ({backHref, color, title, subtitle}: Props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <Link href={backHref}>
+          <BackIcon color={color} />
+        </Link>
+      </div>
+      <div className={styles.center}>
+        {title &&
+          <div className={styles.title}>{title}</div>
+        }
+        {subtitle &&
+          <div className={styles.subtitle}>{subtitle}</div>
+        }
+      </div>
+      <div className={styles.rightSide}></div>
+    </div>
+  );
+}
--- pages/[tenant]/login.tsx
+++ pages/[tenant]/login.tsx
@@ -1,6 +1,6 @@
 //package imports
 import { GetServerSideProps } from 'next';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 //style imports
 import styles from '../../styles/Login.module.css';
@@ -10,6 +10,8 @@ import { useApi } from '../../libs/useApi';
 import { Tenant } from '../../types/Tenant';
 import { useAppContext } from '../../contexts/AppContext';
 import Head from 'next/head';
+import { Header } from '../../components/Header';
+import { InputField } from '../../components/InputField';
 
 const Login = (data: Props) => {
   const {tenant, setTenant} = useAppContext();
@@ -18,11 +20,31 @@ const Login = (data: Props) => {
     setTenant(data.tenant);
   }, []);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className={styles.container}>      
       <Head>
         <title>Login | {data.tenant.name}</title>
       </Head>
+
+      <Header color={data.tenant.mainColor} backHref={`/${data.tenant.slug}`} />
+
+      <InputField
+        color={data.tenant.mainColor}
+        placeholder="Digite Seu E-Mail"
+        value={email}
+        onChange={setEmail}
+      />
+
+      <InputField
+        color={data.tenant.mainColor}
+        placeholder="Digite Sua senha"
+        value={password}
+        onChange={setPassword}
+        password
+      />
     </div>
   );
 }
