6f37e13c039bd89a413a1ceb05e66368367288de
Rafael Lisot <rafael.lisot@unoesc.edu.br>
2022-10-08 22:25:29 -0300

    Colocando icones para componentes

--- components/Icon/index.tsx
+++ components/Icon/index.tsx
@@ -1,5 +1,11 @@
 //image imports
+import Card from './card.svg';
+import Checked from './checked.svg';
+import Cupom from './cupom.svg';
+import Location from './location.svg';
 import MailSent from './mailSent.svg';
+import Money from './money.svg';
+import RightArrow from './rightArrow.svg';
 
 type Props = {
   icon: string;
@@ -11,7 +17,13 @@ type Props = {
 export const Icon = ({icon, color, width, height}: Props) => {
   return (
     <div style={{width, height}}>
+      {icon === 'card' && <Card color={color} />}
+      {icon === 'checked' && <Checked color={color} />}
+      {icon === 'cupom' && <Cupom color={color} />}
+      {icon === 'location' && <Location color={color} />}
       {icon === 'mailSent' && <MailSent color={color} />}
+      {icon === 'money' && <Money color={color} />}
+      {icon === 'rightArrow' && <RightArrow color={color} />}
     </div>
   )
 }
